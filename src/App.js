d09288155aeef24a2601a527b4d7f756dba7f1e2
Carlos Banks <cbanks@clypr.co>
2021-11-21 15:53:45 -0500

    initial commit - heroku deployed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 // Constants
@@ -6,11 +7,44 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+const App = () => {
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">🖼 GIF Portal - by: Carlos Banks</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
